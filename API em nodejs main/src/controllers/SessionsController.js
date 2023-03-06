d26ad5378443528669d8f2d19df6a0ab674db92f
RichardOrlandini <richardorlandini09@gmail.com>
2023-03-06 15:13:27 -0300

    ajuste sessions token

--- API em nodejs main/src/controllers/SessionsController.js
+++ API em nodejs main/src/controllers/SessionsController.js
@@ -8,22 +8,26 @@ const { sign } = require("jsonwebtoken");
 class SessionsController { 
     async create(request, response){
         const { email, password} = request.body;
-        console.log(email, password)
 
-        const user = await knex("users").where({email}).then(e => console.log(e))
+        const user = await knex("users").where({email}).first();
 
         if (!user){
             throw new AppError("usuário não cadastrado", 401);
         }
 
-        const passwordMatched = await compare(password, user.password);
+        const passwordMatched = await compare(password, user.password)
 
         if (!passwordMatched){
             throw new AppError("E-mail e/ou senha incorreta", 401);
         }
-        
 
-        return response.json(user);
+        const {secret, expiresIn} = authConfig.jwt;
+        const token = sign({}, secret, {
+            subject: String(user.id),
+            expiresIn
+        })
+
+        return response.json({user, token});
     }
 };
 
